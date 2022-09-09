e80d184f6c4d46933b6fa9ae07bec3d201d1c72c
Andrei Sibisan <andrei.sibisan@gmail.com>
2022-09-09 20:43:59 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,22 +5,6 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
-}
-
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger.</h1>;
-}
-
-const user = {
-  firstName: "Andrei",
-  lastName: "Sibisan",
-};
-
 const element = (
   <div>
     <h1>Hello!</h1>
