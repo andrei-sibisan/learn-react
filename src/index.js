fb911dd5e4ee89843e93af5e1e81611805bec090
Andrei Sibisan <andrei.sibisan@gmail.com>
2022-09-09 21:51:27 +0300

    Composing components

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+const element = App();
+root.render(element);
